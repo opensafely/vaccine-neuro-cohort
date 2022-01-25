36c8a423c79b276a03bf00794e64ad293d0b6c47
annaschultze <59482746+annaschultze@users.noreply.github.com>
2022-01-25 18:23:01 +0000

    implement matching

---
+++ analysis/matching.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from osmatching import match
+
+# CONCURRENT CONTROLS
+match(
+    case_csv="input_exposed",
+    match_csv="input_concurrent_controls",
+    matches_per_case=5,
+    match_variables={
+        "matching_age": "category",
+        "stp": "category",
+    },
+    index_date_variable="first_known_vaccine_date", 
+    replace_match_index_date_with_case="no_offset", 
+    date_exclusion_variables={
+        "death_date": "before",
+        "dereg_date": "before",
+        "first_any_vaccine_date": "before",
+    },
+    indicator_variable_name="exposed", 
+    output_suffix="_concurrent",
+    output_path="output",
+)
